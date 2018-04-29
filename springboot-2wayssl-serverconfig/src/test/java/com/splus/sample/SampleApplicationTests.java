2e3371621c04e2b2e724d555d7de6ad8ed20e095
Heraj.Mishra <Heraj.Mishra@SP01-LPT-0143.splus.local>
2018-04-29 11:53:28 +0530

    changes for junit

--- springboot-2wayssl-serverconfig/src/test/java/com/splus/sample/SampleApplicationTests.java
+++ springboot-2wayssl-serverconfig/src/test/java/com/splus/sample/SampleApplicationTests.java
@@ -2,11 +2,9 @@ package com.splus.sample;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class SampleApplicationTests {
 
 	@Test
